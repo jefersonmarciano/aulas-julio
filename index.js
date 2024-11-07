54252eba242e223d31a350dc509cf497e0229f02
Jeferson marciano <44215511+jefersonmarciano@users.noreply.github.com>
2024-11-07 10:59:40 -0300

    adicionando aulas

---
+++ index.js
@@ -0,0 +1,29 @@
+/* 1 - Declare uma variável chamado anoAtual e atribua a ela o valor do ano atual. */
+
+const anoAtual = '2024';
+
+/* 2 - Declare uma variável chamada numeros e atribua a ela um número decimal, por exemplo, 3.14. */
+
+let numeros = 3.14;
+
+/* 3 - Declare uma variável chamada cidade e outra chamada estado, ambas do tipo string. Atribua a elas o nome de uma cidade e um estado à sua escolha. */
+
+const cidade = 'Bauru';
+const estado = 'São Paulo';
+
+/* 4 - Declare uma variável chamada isOnline e atribua a ela o valor booleano true, representando se uma pessoa está online. */
+
+const isOnline = true;
+
+/* 5 - Declare uma variável chamada distancia e atribua a ela um valor numérico em quilômetros, representando uma distância. */
+
+let distancia = 400;
+
+/* 6 - Concatene a variável cidade e estado em uma variável só, chamada cidadeEstado que será a combinação das duas variáveis. 
+Na seguinte ordem cidade + estado. Sem espaços */
+
+const cidadeEstado = `${cidade}${estado}`;
+
+/* 7 - Declare uma variável chamada parOuImpar que receba um valor e determine se é par ou impar e caso seja par atribua o valor "par" caso seja impar atribua o valor "impar". Use o operador de resto e o operador ternário */
+
+let parOuImpar = 10 % 5 == 0 ? 'par' : 'impar';
